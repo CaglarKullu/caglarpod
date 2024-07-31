01b27d1f8b47c2c370aec6d153fa59449b012409
Caglar Kullu <caglarkullu@gmail.com>
2024-07-31 17:05:09 +0100

    Release version 1.0.3

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export {default as StateNotifier } from './core/StateNotifier';
-export type { default as PersistenceLayer } from './core/StateNotifier';
-export { default as useStateNotifier } from './core/useStateNotifier';
-export { default as useQuery } from './core/useQuery';
-export {default as useMutation } from './core/useMutation';
-export {default as useStreamQuery } from './core/useStreamQuery';
-export {default as useStateSelector } from './core/useStateSelector';
-export {default as ProviderScope } from './core/ProviderScope';
