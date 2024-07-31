9bb009310c239615635d5225264d8632a02898be
Caglar Kullu <caglarkullu@gmail.com>
2024-07-31 16:43:56 +0100

    Release version 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { StateNotifier } from './core/StateNotifier';
-export type { PersistenceLayer } from './core/StateNotifier';
-export { useStateNotifier } from './core/useStateNotifier';
-export { useQuery } from './core/useQuery';
-export { useMutation } from './core/useMutation';
-export { useStreamQuery } from './core/useStreamQuery';
-export { useStateSelector } from './core/useStateSelector';
-export { default as ProviderScope } from './core/ProviderScope';
+export {default as StateNotifier } from './core/StateNotifier';
+export type { default as PersistenceLayer } from './core/StateNotifier';
+export { default as useStateNotifier } from './core/useStateNotifier';
+export { default as useQuery } from './core/useQuery';
+export {default as useMutation } from './core/useMutation';
+export {default as useStreamQuery } from './core/useStreamQuery';
+export {default as useStateSelector } from './core/useStateSelector';
+export {default as ProviderScope } from './core/ProviderScope';
